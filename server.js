6c3e7e5421eb312d80fc68cc28fbf21c67b5b2ac
Waterbug <amoseman18@gmail.com>
2021-05-26 17:03:44 -0600

    added models and routes folder and connection to mongoose in server

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const routes = require('./routes')
+
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
@@ -10,6 +13,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/movieDB");
 
 // Define API routes here
 
